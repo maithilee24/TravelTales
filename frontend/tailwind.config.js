c9f1637c35f9f5c745d4b9237e774decd6b76e1c
maithilee <pardhia.ug22.ec@nitp.ac.in>
2025-03-29 15:49:47 +0530

    resolved tailwindcss issue

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./context/**/*.{js,ts,jsx,tsx,mdx}",
+    "./hooks/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "rgb(245, 230, 239)",
+      },
+    },
+  },
+  plugins: [],
+}
