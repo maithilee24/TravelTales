f5ad1f4a279c8cf5832529305cb938a57d55be96
maithilee <pardhia.ug22.ec@nitp.ac.in>
2025-03-29 15:14:25 +0530

    ChangePassword added

---
+++ frontend/app/change-password/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ChangePasswordForm from "../Components/auth/ChangePasswordForm/ChangePasswordForm";
+
+function page() {
+    return <div className="bg-[#082026] w-full min-h-screen flex justify-center items-center">
+        <ChangePasswordForm />
+    </div>;
+}
+
+export default page;
