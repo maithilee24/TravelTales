a8196e40611805535a4ebf799a0718135ed877cf
maithilee <pardhia.ug22.ec@nitp.ac.in>
2025-03-29 15:01:13 +0530

    Login Page Added

---
+++ frontend/app/login/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import LoginForm from "../Components/auth/LoginForm/LoginForm";
+
+function page() {
+    return <div className="bg-[#082026] w-full min-h-screen flex justify-center items-center">
+        <LoginForm />
+    </div>;
+}
+
+export default page;
---
+++ frontend/providers/UserProvider.tsx
@@ -0,0 +1,14 @@
+"use client";
+import React from 'react';
+import {UserContextProvider} from "../context/userContext.js"
+
+interface Props {
+    children: React.ReactNode;
+}
+function UserProvider({children}: Props){
+    return (
+        <UserContextProvider>{children}</UserContextProvider>
+    )
+}
+
+export default UserProvider;
