c720b6eee40e6accfa684f76708a98a4e8f87a58
maithilee <pardhia.ug22.ec@nitp.ac.in>
2025-03-29 15:02:54 +0530

    Register Page added

---
+++ frontend/app/register/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import RegisterForm from "../Components/auth/RegisterForm/RegisterForm";
+
+function page() {
+    return <div className="bg-[#082026] w-full min-h-screen flex justify-center items-center">
+        <RegisterForm />
+    </div>;
+}
+
+export default page;
